7f4d1b4640adddb44534a3706e9f46ca067fd9e6
Arseniia-Damaksina <arseniiadamaxcoop@gmail.com>
2024-03-26 15:56:14 +0200

    added validation pipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+  
   const configService = app.get(ConfigService);
+
   const port = configService.get('port');
   await app.listen(port);
 }
