bdd53efe29ccbdc00ad483403a0a39292a66c207
Arseniia-Damaksina <arseniiadamaxcoop@gmail.com>
2024-04-01 18:40:31 +0300

    type issue

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -3,3 +3,7 @@ declare module "*.png";
 declare module "*.svg";
 declare module "*.jpeg";
 declare module "*.jpg";
+declare module "react-dom/client" {
+    var createRoot: any;
+    export {createRoot};
+  }
